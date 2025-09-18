a547755398eae5eb09b5096b5b710f72844c736f
Z_R <mandarinzita7@gmail.com>
2025-09-18 11:56:11 +0500

    still test

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Размер нашего массива
+#define N 288
+
+int main()
+{
+    printf("=== Лабораторная работа №1 ===\n");
+    printf("Датчик печенья в умном доме\n\n");
+
+    // Здесь будет наш код
+
+    return 0;
+}
